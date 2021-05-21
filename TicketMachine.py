04b55d163c56c419d1fb8066f93bf4d850c1634a
Michał Słota <irreftw@gmail.com>
2021-05-21 19:03:54 +0200

    only tests left

--- TicketMachine.py
+++ TicketMachine.py
@@ -7,9 +7,9 @@ class TicketMachine(CoinContainer):
         "reduced20mTicket": 2.20,
         "reduced40mTicket": 4.70,
         "reduced60mTicket": 7.50,
-        "normal20mTicket ": 3.70,
-        "normal40mTicket ": 6.20,
-        "normal60mTicket ": 10.2
+        "normal20mTicket": 3.70,
+        "normal40mTicket": 6.20,
+        "normal60mTicket": 10.2
     }
 
     ticketsPrice = 0
@@ -18,22 +18,7 @@ class TicketMachine(CoinContainer):
         super().__init__()
 
     def addTicket(self, ticket, amount=1):
-        print("testasdf")
-        self.ticketsPrice = Decimal(self.ticketsPrice+ self.avaiableTickets[str(ticket)] * amount)
+        self.ticketsPrice = Decimal(self.ticketsPrice+ Decimal(self.avaiableTickets[str(ticket)]) * amount)
 
     def showCostOfTickets(self):
         print("total cost of tickets :", self.ticketsPrice)
-
-    def change(self, userListOfCoins):  # generalnie podaje mu liste monet wrzuconą przez usera
-        if self.ticketsPrice == Decimal(sum(userListOfCoins)):
-            print("Thank you citzen!")
-            self.coins_list = self.coins_list + userListOfCoins
-        elif self.ticketsPrice < self.sumOfCoins():  # wydaje reszte
-            change = Decimal(sum(userListOfCoins)) - Decimal(self.ticketsPrice)
-            self.coins_list.sort(reverse=True) #TODO:to musze dokonczyc
-            print(self.coins_list)
-            # while change > 0 :
-
-        elif self.ticketsPrice > self.sumOfCoins():
-            print("Tylko odliczona kwota")
-            return userListOfCoins
